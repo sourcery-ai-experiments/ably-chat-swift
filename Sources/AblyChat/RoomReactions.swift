b36db1bbc4ce49b1bb8f9788984e216133468c5f
Lawrence Forooghian <lawrence@forooghian.com>
2024-09-09 21:41:59 -0300

    Fix signature of RoomReactions.send(params:)
    Marat noticed this mistake in 20e7f5f; I got the name of the type wrong
    and I forgot to add its properties.

--- Sources/AblyChat/RoomReactions.swift
+++ Sources/AblyChat/RoomReactions.swift
@@ -1,11 +1,19 @@
 import Ably
 
 public protocol RoomReactions: AnyObject, Sendable, EmitsDiscontinuities {
-    func send(params: RoomReactionParams) async throws
+    func send(params: SendReactionParams) async throws
     func subscribe(bufferingPolicy: BufferingPolicy) -> Subscription<Reaction>
     var channel: ARTRealtimeChannelProtocol { get }
 }
 
-public struct RoomReactionParams: Sendable {
-    public init() {}
+public struct SendReactionParams: Sendable {
+    public var type: String
+    public var metadata: ReactionMetadata?
+    public var headers: ReactionHeaders?
+
+    public init(type: String, metadata: ReactionMetadata? = nil, headers: ReactionHeaders? = nil) {
+        self.type = type
+        self.metadata = metadata
+        self.headers = headers
+    }
 }
